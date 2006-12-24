404432709d0ef32dd84b9c4af7bcc5cc054fb9b7
subwiz <subwiz@07c8daec-9f0b-0410-ab0e-8e3faf876ee5>
2006-12-24 14:32:08 +0000

    v2.1 tag created

---
+++ tags/2.1/src/main/java/org/wiztools/wizcrypt/Decrypt.java
@@ -0,0 +1,51 @@
+package org.wiztools.wizcrypt;
+
+import javax.crypto.NoSuchPaddingException;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.InvalidKeyException;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import java.util.ResourceBundle;
+
+/**
+ * Class to do the decryption using the WizCrypt naming convention (*.wiz).
+ */
+public class Decrypt implements IProcess{
+    
+    private static final ResourceBundle rb = ResourceBundle.getBundle("org.wiztools.wizcrypt.wizcryptmsg");
+    
+    private CipherKey ce;
+    
+    public void init(final String keyStr) throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException{
+        ce = CipherKeyGen.getCipherKeyForDecrypt(keyStr);
+    }
+    
+    public void process(final File file) throws IOException, FileNotFoundException, PasswordMismatchException{
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        boolean canDelete = false;
+        try{
+            String path = file.getCanonicalPath();
+            if(!path.endsWith(".wiz")){
+                throw new FileNotFoundException(rb.getString("err.file.not.end.wiz")+path);
+            }
+            String newPath = path.replaceFirst(".wiz$", "");
+            File outFile = new File(newPath);
+            fis = new FileInputStream(file);
+            fos = new FileOutputStream(outFile);
+            WizCrypt.decrypt(fis, fos, ce);
+            canDelete = true;
+        } finally{
+            // fos & fis will be closed by WizCrypt.decrypt() API
+            if(canDelete){
+                file.delete();
+            }
+        }
+    }
+}
